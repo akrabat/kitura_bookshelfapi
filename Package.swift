f4df874fa27c3cad6e7ea80e4a0de007f561726e
Rob Allen <rob@akrabat.com>
2016-10-06 11:28:10 +0100

    V1 of SwiftDotEnv

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/SwiftOnTheServer/SwiftDotEnv.git", majorVersion: 0),
+        .Package(url: "https://github.com/SwiftOnTheServer/SwiftDotEnv.git", majorVersion: 1),
     ]
 )
